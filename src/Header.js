b82b3fc93db734176ccc44a8ef15bb670c6ec806
91639 <goel.aryan2406@gmail.com>
2021-11-16 11:10:54 +0530

    header completed

--- src/Header.js
+++ src/Header.js
@@ -2,18 +2,20 @@ import React from 'react'
 import './Header.css';
 import PersonIcon from '@mui/icons-material/Person';
 import  {IconButton}  from '@mui/material';
+import ForumIcon from '@mui/icons-material/Forum';
 import logo from './images/tinder.png';
 function header() {
     return (
         <div className="header">
             <IconButton><PersonIcon  fontSize="large" className="header_icon"/> </IconButton>
             <img className="header_logo" 
-            src={logo} alt=""> 
-            
-             </img>
-            
+            src={logo} 
+            alt=""       
+             />
+             <IconButton> 
+    <ForumIcon fontSize="large" className="header_icon"/> </IconButton>
         </div>
-    )
+    );
 }
 
 export default header
---
+++ src/tinderCards.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./tinderCards.css";
+
+function tinderCards() {
+    return (
+        <div className="tinderCards">
+            
+        </div>
+    )
+}
+
+export default tinderCards
