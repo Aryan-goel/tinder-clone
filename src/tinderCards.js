2a066f231318d8ae338b8df69798ef319d7605f9
91639 <goel.aryan2406@gmail.com>
2021-11-17 15:26:27 +0530

    card updates

--- src/tinderCards.js
+++ src/tinderCards.js
@@ -1,23 +1,49 @@
+import { SwipeableDrawer } from "@mui/material";
 import React, { useState } from "react";
+import TinderCard from "react-tinder-card";
 import "./tinderCards.css";
 
 function TinderCards() {
 const [people,setPeople]=useState([
     {
         name:'Elon musk',
-        url:"https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F8%2F85%2FElon_Musk_Royal_Society_%2528crop1%2529.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FElon_Musk&tbnid=zY4uP_4PDKQQGM&vet=12ahUKEwiR_8uA85z0AhUQlUsFHZg4BCUQMygAegUIARDNAQ..i&docid=cElLiMk8T9O8LM&w=480&h=720&itg=1&q=elon%20musk&ved=2ahUKEwiR_8uA85z0AhUQlUsFHZg4BCUQMygAegUIARDNAQ",
+        url:"https://www.usnews.com/dims4/USNEWS/f45ea7c/2147483647/thumbnail/640x420/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2Fd1%2Fd8%2F8501ba714a21aed9a7327e02ade1%2F180515-10thingselonmusk-editorial.jpg",
     },
     {
         name:'Jeff bezos',
-        url:'sdfsd'
+        url:'https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5f469ea85cc82fc8d6083f05%2FAmazon-Founder-and-CEO-Jeff-Bezos%2F960x0.jpg%3Ffit%3Dscale'
     }
 ]);
+const Swiped=(direction,nameToDelete)=>{
+    console.log("removing:"+nameToDelete);
+    
+};
+const outOfFrame=(name)=>{
+    console.log(name+"left the screen");
+}
 
     return (
         <div className="tinderCards">
+           <div className="tinderCards__cardContainer">
            {people.map((person)=>(
-               <h1>{person.name}</h1>
+               <TinderCard
+               className="swipe"
+               key={person.name}
+               preventSwipe={["up","down"]}
+               onSwipe={(dir)=> Swiped(dir,person.name)}
+               onCardLeftScreen={()=> outOfFrame(person.name)}
+            > 
+               <div
+               style={{ backgroundImage: `url(${person.url})`}}
+               className="card"
+               >
+               <h3>{person.name}</h3>    
+               </div>
+               
+                </TinderCard>
            ))}
+           </div>
+           
         </div>
     );
 }
